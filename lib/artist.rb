a28b5957cae274277e5123e4bb0d4b8dd64ae6e7
Roberto Samia <bojosteph@gmail.com>
2018-11-18 00:23:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,23 @@
 def Artist
   attr_accessor :name
   
-  def initialize(b)
+ def initialize(name)
   @name = name
+  @songs = []
+end 
+
+def add_song(song)
+  @songs << song
+  song.artist = self
   
+end
+
+def songs
+  @songs
+end
+
+
+def add_song_by_name(name, genre)
   
   
 end
