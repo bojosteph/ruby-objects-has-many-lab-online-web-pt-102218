2b6fa6b82f67047c376469c79262f3bb3ed864a3
Roberto Samia <bojosteph@gmail.com>
2018-11-18 01:57:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,7 +12,7 @@ end
 def add_song(song)
   @songs << song
   song.artist = self
-  @@song_count += 1
+ # @@song_count += 1
 end
 
 def songs
@@ -24,6 +24,7 @@ def add_song_by_name(name)
   song = Song.new(name)
   @songs << song
   song.artist = self
+  @@song_count += 1
 end
 
 
