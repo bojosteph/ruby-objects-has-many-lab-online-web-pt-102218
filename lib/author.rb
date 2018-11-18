aa5fda64bbdeb94bc4b66f8e7365f99095dccabe
Roberto Samia <bojosteph@gmail.com>
2018-11-18 02:07:01 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,14 +1,25 @@
 class Author
   
-  attr_accessor :name, :post
+  attr_accessor :name, :posts
+  @@post_count = 0
   
   def initialize(name)
     @name = name
-    @post = []
+    @posts = []
   end
   
-  def add_post(post)
-    @post << post
+  def add_post(posts)
+    @posts << posts
+    posts.author = self
+    @@post_count += 1 
+  end
+  
+  def posts
+    @posts
+  end
+  
+  def add_post_by_title(name)
+    posts = P
   
   
   
